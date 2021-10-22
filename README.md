862903e1e805dc71b36d3246550ab0acecc677d0
Salt Fish <526191197@qq.com>
2021-10-22 23:17:25 +0800

    msgp example

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# lab
+# msgp example
+
+This is a simple example of msgp.
+
+[msgp](https://github.com/tinylib/msgp) is a code generation tool and serialization library
+for [MessagePack](http://msgpack.org).
+
+Run `go mod tidy`, `make generate`, `make build`, `make run` to see output.
--- scripts/release.sh
+++ scripts/release.sh
@@ -4,6 +4,6 @@ set -o nounset
 set -o pipefail
 
 go mod tidy
-mkdir -p "$WORKDIR"/output/bin/ && go build -ldflags "-X $APP_NAME/config.BuildDate=$(date '+%F_%H:%M:%S')" -o "$WORKDIR"/output/bin/"$APP_NAME" .
+mkdir -p "$WORKDIR"/output/bin/ && go build -ldflags "-X $APP_NAME/config.BuildDate=$(date '+%F_%H:%M:%S') -X $APP_NAME/config.Release=true" -o "$WORKDIR"/output/bin/"$APP_NAME" .
 echo "#!/bin/bash" >"$WORKDIR"/output/run.sh
 echo "${WORKDIR}/output/bin/${APP_NAME}" >>"$WORKDIR"/output/run.sh
