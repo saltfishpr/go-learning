d62617fca0c4eb11ae36157e4b1f6a27f5033503
saltfishpr <526191197@qq.com>
2023-04-06 21:51:48 +0800

    Update

--- pkg/auth/jwt/jwt.go
+++ pkg/auth/jwt/jwt.go
@@ -2,8 +2,6 @@ package jwt
 
 import (
 	"context"
-
-	jwtV4 "github.com/golang-jwt/jwt/v4"
 )
 
 // Auth 自定义认证信息.
@@ -21,15 +19,4 @@ func (c Auth) GetRequestMetadata(ctx context.Context, uri ...string) (map[string
 // RequireTransportSecurity 是否需要安全传输.
 func (c Auth) RequireTransportSecurity() bool {
 	return false
-}
-
-var contextKeyClaims int
-
-func NewContext(ctx context.Context, claims *jwtV4.MapClaims) context.Context {
-	return context.WithValue(ctx, &contextKeyClaims, claims)
-}
-
-func FromContext(ctx context.Context) (*jwtV4.MapClaims, bool) {
-	claims, ok := ctx.Value(&contextKeyClaims).(*jwtV4.MapClaims)
-	return claims, ok
 }
--- pkg/ctxutil/ctxutil.go
+++ pkg/ctxutil/ctxutil.go
@@ -1,1 +1,21 @@
 package ctxutil
+
+import (
+	"context"
+
+	jwtv4 "github.com/golang-jwt/jwt/v4"
+)
+
+var ctxKeyClaims int
+
+func ClaimsToContext(ctx context.Context, claims jwtv4.MapClaims) context.Context {
+	return context.WithValue(ctx, &ctxKeyClaims, claims)
+}
+
+func ClaimsFromContext(ctx context.Context) jwtv4.MapClaims {
+	claims, ok := ctx.Value(&ctxKeyClaims).(jwtv4.MapClaims)
+	if !ok {
+		return nil
+	}
+	return claims
+}
