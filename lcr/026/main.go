6ba3378e1fc7dfab20ee9e8283745aba7e444d5b
saltfishpr <526191197@qq.com>
2024-07-12 15:08:16 +0800

    update

---
+++ lcr/026/main.go
@@ -0,0 +1,36 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reorderList(head *ListNode) {
+	l1, l2 := head, findMid(head)
+	// 分离两个链表
+	l2.Next, l2 = nil, l2.Next
+	// 反转第二个链表
+	l2 = reverseList(l2)
+
+	for l1 != nil && l2 != nil {
+		l1.Next, l2.Next, l1, l2 = l2, l1.Next, l1.Next, l2.Next
+	}
+}
+
+func findMid(head *ListNode) *ListNode {
+	dummy := &ListNode{Next: head}
+	slow, fast := dummy, dummy
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
+func reverseList(head *ListNode) *ListNode {
+	dummy := &ListNode{}
+	for head != nil {
+		dummy.Next, head.Next, head = head, dummy.Next, head.Next
+	}
+	return dummy.Next
+}
---
+++ lcr/027/main.go
@@ -0,0 +1,39 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func isPalindrome(head *ListNode) bool {
+	mid := findMid(head)
+	l1 := head
+	l2 := reverseList(mid.Next)
+	for l2 != nil {
+		if l1.Val != l2.Val {
+			return false
+		}
+		l1 = l1.Next
+		l2 = l2.Next
+	}
+	return true
+}
+
+// findMid 当链表长度为奇数时，返回中间节点；当链表长度为偶数时，返回中间靠前的节点。
+func findMid(head *ListNode) *ListNode {
+	dummy := &ListNode{Next: head}
+	slow, fast := dummy, dummy
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
+func reverseList(head *ListNode) *ListNode {
+	dummy := &ListNode{}
+	for head != nil {
+		dummy.Next, head.Next, head = head, dummy.Next, head.Next
+	}
+	return dummy.Next
+}
