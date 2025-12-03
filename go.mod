df3970a3d6d658e196dd4fd2a69fd1e9be428836
saltfishpr <526191197@qq.com>
2025-12-04 00:12:03 +0800

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module learning
 
-go 1.21
+go 1.24.0
+
+require (
+	github.com/pkg/errors v0.9.1
+	golang.org/x/sys v0.37.0
+)
--- main.go
+++ main.go
@@ -1,5 +1,78 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/pkg/errors"
+	"golang.org/x/sys/windows"
+	"golang.org/x/sys/windows/registry"
+)
+
+type SteamApp struct {
+	AppID     string
+	StoreName string
+}
+
+func getSteamRunningApps() ([]*SteamApp, error) {
+	// 遍历 HKEY_CURRENT_USER\Software\Valve\Steam\Apps
+	appsKey, err := registry.OpenKey(registry.CURRENT_USER, `Software\Valve\Steam\Apps`, registry.READ)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	defer appsKey.Close()
+	appIDs, err := appsKey.ReadSubKeyNames(65536)
+	if err != nil && err != io.EOF {
+		return nil, errors.WithStack(err)
+	}
+
+	var runningApps []*SteamApp
+	for _, appIDStr := range appIDs {
+		app, err := getAppIfRunning(appsKey, appIDStr)
+		if err != nil {
+			log.Printf("Error checking app %s: %v", appIDStr, err)
+			continue
+		}
+		if app != nil {
+			runningApps = append(runningApps, app)
+		}
+	}
+	return runningApps, nil
+}
+
+func getAppIfRunning(appsKey registry.Key, appIDStr string) (*SteamApp, error) {
+	appKey, err := registry.OpenKey(appsKey, appIDStr, registry.READ)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	defer appKey.Close()
+	v, _, err := appKey.GetIntegerValue("Running")
+	if err != nil {
+		if err == windows.ERROR_FILE_NOT_FOUND {
+			return nil, nil
+		}
+		return nil, errors.WithStack(err)
+	}
+	if v != 1 {
+		return nil, nil
+	}
+	storeName, _, err := appKey.GetStringValue("Name")
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return &SteamApp{
+		AppID:     appIDStr,
+		StoreName: storeName,
+	}, nil
+}
+
 func main() {
-	println("Hello, World!")
+	apps, err := getSteamRunningApps()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	for _, app := range apps {
+		fmt.Printf("Running AppID: %s, Name: %s\n", app.AppID, app.StoreName)
+	}
 }
